d365da50b3bf22c83d4592725070c667a326a83f
Rushang Dhanesha <rushangdhanesha@gmail.com>
2019-08-12 13:04:31 +0530

    Change env vars name

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,16 +1,16 @@
 const os = require('os');
 
 const envVariables = {
-    serviceId : process.env.name || 'http-to-kafka',
-    ver : process.env.version || '1.0.0',
+    serviceId : process.env.NAME || 'http-to-kafka',
+    ver : process.env.VERSION || '1.0.0',
 
-    kafkaHost: process.env.kafka_broker_list || 'localhost:9092',
-    topic: process.env.kafka_topic || 'whatsapp-received-messages',
+    kafkaHost: process.env.KAFKA_BROKER_LIST || 'localhost:9092',
+    topic: process.env.KAFKA_TOPIC || 'whatsapp-received-messages',
     maxAsyncRequests: 100,
-    keyForEachMessage : process.env.keyPath || '$.eventContent.message.from',      //In request body, single value
+    keyForEachMessage : process.env.KAFKA_KEY_JSONPATH || '$.eventContent.message.from',      //In request body, single value
 
-    contextPath: process.env.contextPath || '/whatsapp-webhook',
-    port: process.env.service_port || 9001,
-    endPoint : process.env.endPoint || '/whatsapp/messages'
+    contextPath: process.env.SERVER_CONTEXT_PATH || '/whatsapp-webhook',
+    port: process.env.SERVER_PORT || 9001,
+    endPoint : process.env.ENDPOINT || '/whatsapp/messages'
 }
 module.exports = envVariables;
